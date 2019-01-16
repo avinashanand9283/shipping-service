9b0b54b806c767b87eb74b527b5a52eb42f4f444
Avinash Anand <avinashanand@Avinashs-MBP.lan>
2019-01-16 06:50:16 +0100

    Added Lab2 Microservice express

--- tests/product-service.test.js
+++ tests/product-service.test.js
@@ -1,6 +1,6 @@
 /**
-* @jest-environment node
-*/
+ * @jest-environment node
+ */
 var productService = require('../src/services/product-service')
 var nock = require('nock')
 
@@ -17,20 +17,21 @@ describe('Product service', function () {
     let weight = await productService.getProductWeight('13')
     expect(weight).toBe(15.5)
   })
+
   it('Should handle unexpected response structure', async function () {
     nock('https://mycluster.icp:8899/products')
-        .get('/19')
-        .reply(200, {
-          res: 15.5
-        })
-  
+      .get('/19')
+      .reply(200, {
+        res: 15.5
+      })
+
     await productService
-        .getProductWeight('19')
-        .then(() => {
-          throw(new Error('Should not resolve in case of malformed data'))
-        })
-        .catch(err => {
-          expect(err.message).toBe('Invalid response object')
-        })
-    })
+      .getProductWeight('19')
+      .then(() => {
+        throw (new Error('Should not resolve in case of malformed data'))
+      })
+      .catch(err => {
+        expect(err.message).toBe('Invalid response object');
+      })
+  })
 })
